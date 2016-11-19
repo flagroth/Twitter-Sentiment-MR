29f026fbaa960d4414118d0ffef723b9013faf94
flagroth <javier.cano.montero@urjc.es>
2016-11-19 10:49:50 +0100

    Mapper limpio
    El mapper solo devuelve letras

--- streaming_mapper.py
+++ streaming_mapper.py
@@ -2,19 +2,38 @@
 
 import sys
 import json
+import unicodedata
+import re, string
+
+
+def elimina_tildes(cadena):
+    s = ''.join((c for c in unicodedata.normalize('NFD',unicode(cadena)) if unicodedata.category(c) != 'Mn'))
+    return s.decode()
+    
+def solo_letras ( text ):
+    regex = re.compile('[^a-zA-Z]')
+    return regex.sub('',text)
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 # Read each line from STDIN
 for line in sys.stdin:
-   record = json.loads(line,encoding='latin-1')
-   # Get the words in each line
-   line = record["text"]
-   line = line.encode('utf8')
-   words = line.split()
+    try:
+        record = json.loads(line,encoding='latin-1')
+    except ValueError:
+        pass
+    if record["lang"]=="es":
+        # Get the words in each line
+        line = record["text"]
+        line = line.encode('utf8')
+        words = line.split()
 
-   # Generate the count for each word
-   for word in words:
-
-      # Write the key-value pair to STDOUT to be processed by the reducer.
-      # The key is anything before the first tab character and the value is
-      # anything after the first tab character.
-      print '{0}\t{1}'.format(word, 1)
+        # Generate the count for each word
+        for word in words:  
+            word = elimina_tildes(word)  
+            word = solo_letras(word)
+            # Write the key-value pair to STDOUT to be processed by the reducer.
+            # The key is anything before the first tab character and the value is
+            # anything after the first tab character.
+            print '{0}\t{1}'.format(word, 1)
