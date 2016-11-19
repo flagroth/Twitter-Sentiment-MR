f8b7052cf4ea1a251669eab02b9941c09bc13184
flagroth <javier.cano.montero@urjc.es>
2016-11-19 17:03:40 +0100

    CP con OSM

--- streaming_mapper.py
+++ streaming_mapper.py
@@ -6,8 +6,9 @@ import unicodedata
 import re, string
 import csv
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 
-gmaps = Nominatim()
+gmaps = Nominatim(timeout=1)
 
 def elimina_tildes(cadena):
     s = ''.join((c for c in unicodedata.normalize('NFD',unicode(cadena)) if unicodedata.category(c) != 'Mn'))
@@ -42,10 +43,14 @@ for line in sys.stdin:
             lon = str(coords["coordinates"][0])
             lat = str(coords["coordinates"][1])
             try:
-                location = gmaps.reverse((lat, lon))
+                location = gmaps.reverse((lat, lon),timeout=1)
+            except GeocoderTimedOut as e:
+                print "Error: geocode failed on input %s with message %s"%(location, e.msg)
+            try:
                 cp = location.raw["address"]["postcode"]
-            except IndexError:
+            except KeyError:
                 pass
+
             words = line.split()
 
             valoracion = 0
