aef5e834814420fa98dbc04dbbcae37aaab90ca7
flagroth <javier.cano.montero@urjc.es>
2016-11-19 13:33:56 +0100

    Sentimientos por CP

--- streaming_mapper.py
+++ streaming_mapper.py
@@ -5,7 +5,9 @@ import json
 import unicodedata
 import re, string
 import csv
+import googlemaps
 
+gmaps = googlemaps.Client(key='AIzaSyAcyQmdMb6gQB3ZfC70d9LFUWC1wBNDTHY')
 
 def elimina_tildes(cadena):
     s = ''.join((c for c in unicodedata.normalize('NFD',unicode(cadena)) if unicodedata.category(c) != 'Mn'))
@@ -27,6 +29,7 @@ for p in palabras:
 # Read each line from STDIN
 t = 0
 for line in sys.stdin:
+    line = line.encode('utf8')
     try:
         record = json.loads(line,encoding='latin-1')
     except ValueError:
@@ -34,18 +37,26 @@ for line in sys.stdin:
     if record["lang"]=="es":
         # Get the words in each line
         line = record["text"]
-        line = line.encode('utf8')
-        words = line.split()
+        coords = record["coordinates"]
+        if coords is not None:
+            lon = str(coords["coordinates"][0])
+            lat = str(coords["coordinates"][1])
+            try:
+                reverse_geocode = gmaps.reverse_geocode((lat, lon),language='es',result_type='postal_code')
+                cp = reverse_geocode[0]["address_components"][0]["short_name"]
+            except IndexError:
+                pass
+            words = line.split()
 
-        t = t+1
-        valoracion = 0
-        for word in words:  
-            word = elimina_tildes(word)  
-            word = solo_letras(word)
-            if sentimientos.has_key(word):
-                valoracion = valoracion+float(sentimientos[word])
-            # Write the key-value pair to STDOUT to be processed by the reducer.
-            # The key is anything before the first tab character and the value is
-            # anything after the first tab character.
+            valoracion = 0
+            for word in words:  
+                word = elimina_tildes(word)  
+                word = solo_letras(word)
+                if sentimientos.has_key(word):
+                    valoracion = valoracion+float(sentimientos[word])
+                # Write the key-value pair to STDOUT to be processed by the reducer.
+                # The key is anything before the first tab character and the value is
+                # anything after the first tab character.
+
+            print '{0}\t{1}'.format(cp,valoracion)
                 
-        print '{0}\t{1}'.format(t, valoracion)
